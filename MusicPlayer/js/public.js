c4da7e9b4057b27d15a2a558105b716513a25b8e
wandering0 <46771580+wandering0@users.noreply.github.com>
2019-06-16 20:58:59 +0800

    Add files via upload

---
+++ MusicPlayer/js/public.js
@@ -0,0 +1,22 @@
+var script = document.createElement("script");
+script.type = "text/javascript";
+script.src = "/js/jquery-2.1.0.js";
+
+document.getElementsByTagName('head')[0].appendChild(script);
+
+$(document).ready(function() {
+	$("#login").click(function() {
+		$("#btn_loginOrRe").text("登录")
+		$("#confirmPwd").remove()
+	})
+	$("#register").click(function() {
+		$("#btn_loginOrRe").text("注册")
+		var formGroup = '<div class="form-group" id="confirmPwd">' +
+			'<label for="message-text" class="control-label">确认密码:</label>' +
+			'<input type="text" class="form-control" id="message-text"/>' +
+			'</div>'
+
+		if($(".modal-body").find("form").children().length < 3)
+			$(".modal-body").find("form").append(formGroup)
+	})
+});
